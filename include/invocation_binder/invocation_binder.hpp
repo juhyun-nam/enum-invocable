03112079d67d09b0b714a1a49b4f2eeaba40a82c
nam <nothinis@naver.com>
2020-05-28 08:45:18 +0900

    clang-format

---
+++ include/invocation_binder/invocation_binder.hpp
@@ -0,0 +1,13 @@
+/**
+ * @file invocation_binder.hpp
+ * @brief invocation_binder main header file
+ * @author juhyun-nam
+ */
+
+#ifndef INVOCATION_BINDER_INVOCATION_BINDER_HPP
+#define INVOCATION_BINDER_INVOCATION_BINDER_HPP
+
+template <typename T>
+class InvocationBinder {};
+
+#endif  // INVOCATION_BINDER_INVOCATION_BINDER_HPP
